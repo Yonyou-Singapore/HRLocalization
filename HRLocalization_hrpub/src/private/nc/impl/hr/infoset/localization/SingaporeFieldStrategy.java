67de21820a3b31f9aad6e94f3cd8b0b029259a82
whyCaiji <wuheyang0617@gmail.com>
2018-10-03 18:06:30 +0800

    Metadata for waitem was screwed and this is the fix

--- HRLocalization_hrpub/src/private/nc/impl/hr/infoset/localization/SingaporeFieldStrategy.java
+++ HRLocalization_hrpub/src/private/nc/impl/hr/infoset/localization/SingaporeFieldStrategy.java
@@ -2,6 +2,7 @@ package nc.impl.hr.infoset.localization;
 
 import nc.itf.hr.infoset.localization.IAddLocalizationFieldStrategy;
 import nc.vo.hr.infoset.InfoSetVO;
+import nc.vo.pub.BusinessException;
 
 /***************************************************************************
  * HR本地化：插入新加坡HR项目需要的人员基本信息字段<br>
@@ -10,6 +11,12 @@ import nc.vo.hr.infoset.InfoSetVO;
  ***************************************************************************/
 public class SingaporeFieldStrategy extends AddFieldAbstractStrategy implements IAddLocalizationFieldStrategy {
 
+	private static final String COUNTRY_CODE = "SG";
+	
+	public SingaporeFieldStrategy() throws BusinessException {
+		defdocMap = getDefdocList();
+	}
+	
 	@Override
 	public InfoSetVO[] addLocalField(InfoSetVO[] vos) {
 		// TODO To add Singapore implementation for the fields
