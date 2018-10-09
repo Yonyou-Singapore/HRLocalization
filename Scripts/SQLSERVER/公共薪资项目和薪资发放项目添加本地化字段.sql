a40b87feb8611905e9cb564c766506712e6efc73
whyCaiji <wuheyang0617@gmail.com>
2018-10-09 23:00:18 +0800

    Finished enum for the waitem and applied multi-language fields

--- Scripts/SQLSERVER/公共薪资项目和薪资发放项目添加本地化字段.sql
+++ Scripts/SQLSERVER/公共薪资项目和薪资发放项目添加本地化字段.sql
@@ -16,3 +16,5 @@ alter table wa_classitem add my_ispcb CHAR(1) NULL;
 alter table wa_classitem add my_isnplrate CHAR(1) NULL;
 alter table wa_classitem add my_isotrate CHAR(1) NULL;
 
+select * from md_property order by ts desc
+select * from md_class order by ts desc
