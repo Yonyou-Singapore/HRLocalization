4ccbe38fe5ad14a1518a90c2bfb3d913e308490a
whyCaiji <wuheyang0617@gmail.com>
2018-10-18 20:30:56 +0800

    Initial import of WaSignLineNameStrategy for Data Submission to Bank

---
+++ HRLocalization_hrwa/src/client/nc/ui/wa/datainterface/model/WaSignLineNameStrategy.java
@@ -0,0 +1,61 @@
+package nc.ui.wa.datainterface.model;
+
+import java.util.Vector;
+
+import nc.hr.utils.ResHelper;
+import nc.ui.wa.datainterface.view.ModuleItemStrategy;
+import nc.vo.hr.datainterface.DataIOItemVO;
+import nc.vo.hr.datainterface.FieldTypeEnum;
+import nc.vo.wa.datainterface.DataIOconstant;
+
+
+public class WaSignLineNameStrategy implements ModuleItemStrategy{
+//	public static String unitCode = "0";
+//
+//	public static String payoffDate = "1";
+//	public static String payoffNumber = "2";
+	public WaSignLineNameStrategy(){
+
+	}
+
+	public DataIOItemVO[] getCorrespondingItems() {
+
+	        return proFixWa();
+	}
+
+	private DataIOItemVO[] proFixWa()  {
+		Vector<DataIOItemVO> itms = new Vector<DataIOItemVO>();
+//		0  单位代号
+//		1  发放日期
+//		2  发放人数
+		//添加灵活项目
+		DataIOItemVO tmpitem = new DataIOItemVO();
+		tmpitem.setVname(ResHelper.getString("6013bnkitf","06013bnkitf0069")/*@res "单位代号"*/);
+		tmpitem.setPrimaryKey(DataIOconstant.UNITCODE);
+		tmpitem.setIflddecimal(Integer.valueOf(0));
+		tmpitem.setIitemtype((Integer)FieldTypeEnum.STR.value());
+		tmpitem.setIfldwidth(Integer.valueOf(20));
+		itms.add(tmpitem);
+
+		tmpitem = new DataIOItemVO();
+		tmpitem.setVname(ResHelper.getString("6013bnkitf","06013bnkitf0070")/*@res "发放日期"*/);
+		tmpitem.setPrimaryKey(DataIOconstant.DATE);
+		tmpitem.setIflddecimal(Integer.valueOf(0));
+		tmpitem.setIitemtype((Integer)FieldTypeEnum.STR.value());
+		tmpitem.setIfldwidth(Integer.valueOf(10));
+		itms.add(tmpitem);
+
+		tmpitem = new DataIOItemVO();
+		tmpitem.setVname(ResHelper.getString("6013bnkitf","06013bnkitf0071")/*@res "发放人数"*/);
+		tmpitem.setPrimaryKey(DataIOconstant.PSNCOUNT);
+		tmpitem.setIflddecimal(Integer.valueOf(0));
+		tmpitem.setIitemtype((Integer)FieldTypeEnum.STR.value());
+		tmpitem.setIfldwidth(Integer.valueOf(20));
+		itms.add(tmpitem);
+
+
+		DataIOItemVO[] allitemstmp = new DataIOItemVO[itms.size()];
+		return itms.toArray(allitemstmp);
+	}
+
+}
