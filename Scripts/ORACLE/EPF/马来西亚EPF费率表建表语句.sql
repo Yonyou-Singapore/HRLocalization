65b7470b6f226d24f6649012668091b0c5f27243
whyCaiji <wuheyang0617@gmail.com>
2018-10-31 16:23:54 +0800

    Document and Scripts for EPF Contribution Table

---
+++ Scripts/ORACLE/EPF/马来西亚EPF费率表建表语句.sql
@@ -0,0 +1,9 @@
+drop table sealocal_epf_rates;
+create table sealocal_epf_rates
+(
+group_name varchar(200),
+lower decimal(31,8),
+upper decimal(31,8),
+employer_cont decimal(31,8),
+employee_cont decimal(31,8)
+);
