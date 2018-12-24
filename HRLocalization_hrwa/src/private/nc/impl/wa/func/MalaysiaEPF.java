f8e591eceefaa4482f7d99dfa7602d0efaa188cf
whyCaiji <wuheyang0617@gmail.com>
2018-12-24 15:02:33 +0800

    Tweaked the calculation logic of EPF

--- HRLocalization_hrwa/src/private/nc/impl/wa/func/MalaysiaEPF.java
+++ HRLocalization_hrwa/src/private/nc/impl/wa/func/MalaysiaEPF.java
@@ -34,7 +34,7 @@ public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
 		String bonus = SeaLocalFormulaUtil.getConcatString(addRenumItemVOs);
 		
 		if (option.equals("0")) {	// Handle employee contribution
-			sql = getEmployeeContribution(monthlySalary, epfGroup);
+			sql = getEmployeeContribution(monthlySalary, bonus, epfGroup);
 		} else if (option.equals("1")) { // Handle employer contribution
 			sql = getEmployerContribution(monthlySalary, bonus, epfGroup);
 		}
@@ -51,20 +51,20 @@ public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
 	}
 	
 	// Calculating the Employee EPF Contribution
-	private String getEmployeeContribution(String monthlySalary, String epfGroup) {
+	private String getEmployeeContribution(String monthlySalary, String bonus, String epfGroup) {
 		StringBuffer sb = new StringBuffer();
 		sb.append(" select ");
 		// Group A, when monthly wage is over 20000, the employee contribution is wage * 11%
-		sb.append(" case when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + ") * 0.11 ");
+		sb.append(" case when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + bonus + ") * 0.11 ");
 		// Group B, when monthly wage is over 20000, the employee contribution is wage * 11%
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP B' then (" + monthlySalary + ") * 0.11 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP B' then (" + monthlySalary + bonus + ") * 0.11 ");
 		// Group C, when monthly wage is over 20000, the employee contribution is wage * 5.5%
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + ") * 0.055 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + bonus + ") * 0.055 ");
 		// Group D, when monthly wage is over 20000, the employee contribution is wage * 5.5%
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP D' then (" + monthlySalary + ") * 0.055 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP D' then (" + monthlySalary + bonus + ") * 0.055 ");
 		// Else, if wage is not over 20000, strictly follow the EPF table
 		sb.append(" else employee_cont end employee_cont ");
-		sb.append(" from sealocal_epf_rates where " + monthlySalary + " >= lower and " + monthlySalary + " <= upper ");
+		sb.append(" from sealocal_epf_rates where " + monthlySalary + bonus + " >= lower and " + monthlySalary + bonus + " <= upper ");
 		sb.append(" and group_name = " + epfGroup + " ");
 		return sb.toString();
 	}
@@ -75,21 +75,21 @@ public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
 		sb.append(" select ");
 		// Group A, when monthly wage is no more than 5000 and bonus + monthly wage is greater than 5000, contribution is wage * 13%
 		sb.append(" case when " + monthlySalary + " <= 5000 and " + monthlySalary + bonus 
-				+ " > 5000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + ") * 0.13 ");
+				+ " > 5000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + bonus + ") * 0.13 ");
 		// Group C, when monthly wage is no more than 5000 and bonus + monthly wage is greater than 5000, contribution is wage * 6.5%
 		sb.append(" when " + monthlySalary + " <= 5000 and " + monthlySalary + bonus 
-				+ " > 5000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + ") * 0.065 ");
+				+ " > 5000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + bonus + ") * 0.065 ");
 		// Group A, when monthly wage is over 20000, the employer contribution is wage * 12%
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + ") * 0.12 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + bonus + ") * 0.12 ");
 		// Group B, when monthly wage is over 20000, the employer contribution is RM5.00
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP B' then 5.00 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP B' then 5.00 ");
 		// Group C, when monthly wage is over 20000, the employer contribution is wage * 6%
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + ") * 0.06 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + bonus + ") * 0.06 ");
 		// Group D, when monthly wage is over 20000, the employer contribution is RM5.00
-		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP D' then 5.00 ");
+		sb.append(" when " + monthlySalary + bonus + " > 20000 and sealocal_epf_rates.group_name = 'GROUP D' then 5.00 ");
 		// Else, strictly follow the EPF table
 		sb.append(" else employer_cont end employer_cont ");
-		sb.append(" from sealocal_epf_rates where " + monthlySalary + " >= lower and " + monthlySalary + " <= upper ");
+		sb.append(" from sealocal_epf_rates where " + monthlySalary + bonus + " >= lower and " + monthlySalary + bonus + " <= upper ");
 		sb.append(" and group_name = " + epfGroup + " ");
 		return sb.toString();
 	}
