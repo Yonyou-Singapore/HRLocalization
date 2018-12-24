2584013e74790b07a77b7552dcae4bdfbf1f9b0a
whyCaiji <wuheyang0617@gmail.com>
2018-12-24 14:37:54 +0800

    Adding additional formula for EPF normal renumeration contribution

--- HRLocalization_hrwa/src/private/nc/impl/wa/func/MalaysiaEPFNormal.java
+++ HRLocalization_hrwa/src/private/nc/impl/wa/func/MalaysiaEPFNormal.java
@@ -1,20 +1,12 @@
 package nc.impl.wa.func;
 
-import com.ibm.db2.jcc.t4.sb;
-
 import nc.impl.wa.classitem.ClassitemDAO;
 import nc.vo.hr.func.FunctionReplaceVO;
 import nc.vo.pub.BusinessException;
 import nc.vo.wa.classitem.WaClassItemVO;
-import nc.vo.wa.item.PropertyEnumVO;
 import nc.vo.wa.pub.WaLoginContext;
 
-/**
- * HR本地化：EPF后台实现
- * @author Ethan Wu
- *
- */
-public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
+public class MalaysiaEPFNormal extends AbstractPreExcutorFormulaParse {
 
 	@Override
 	public FunctionReplaceVO getReplaceStr(String formula)
@@ -27,16 +19,13 @@ public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
 		ClassitemDAO dao = new ClassitemDAO();
 		WaClassItemVO[] normalRenumItemVOs = dao.queryItemInfoVO(this.getContext().getPk_org(),  this.getContext().getPk_wa_class()
 				, this.getContext().getCyear(), this.getContext().getCperiod(), " wa_classitem.my_isepf_n = 'Y' ");
-		WaClassItemVO[] addRenumItemVOs = dao.queryItemInfoVO(this.getContext().getPk_org(),  this.getContext().getPk_wa_class()
-				, this.getContext().getCyear(), this.getContext().getCperiod(), " wa_classitem.my_isepf_a = 'Y' ");
 		
 		String monthlySalary = SeaLocalFormulaUtil.getConcatString(normalRenumItemVOs);
-		String bonus = SeaLocalFormulaUtil.getConcatString(addRenumItemVOs);
 		
 		if (option.equals("0")) {	// Handle employee contribution
 			sql = getEmployeeContribution(monthlySalary, epfGroup);
 		} else if (option.equals("1")) { // Handle employer contribution
-			sql = getEmployerContribution(monthlySalary, bonus, epfGroup);
+			sql = getEmployerContribution(monthlySalary, epfGroup);
 		}
 		
 		fvo.setReplaceStr(sql);
@@ -70,14 +59,14 @@ public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
 	}
 	
 	// Calculating the Employer EPF 
-	private String getEmployerContribution(String monthlySalary, String bonus, String epfGroup) {
+	private String getEmployerContribution(String monthlySalary, String epfGroup) {
 		StringBuffer sb = new StringBuffer();
 		sb.append(" select ");
 		// Group A, when monthly wage is no more than 5000 and bonus + monthly wage is greater than 5000, contribution is wage * 13%
-		sb.append(" case when " + monthlySalary + " <= 5000 and " + monthlySalary + bonus 
+		sb.append(" case when " + monthlySalary + " <= 5000 and " + monthlySalary
 				+ " > 5000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + ") * 0.13 ");
 		// Group C, when monthly wage is no more than 5000 and bonus + monthly wage is greater than 5000, contribution is wage * 6.5%
-		sb.append(" when " + monthlySalary + " <= 5000 and " + monthlySalary + bonus 
+		sb.append(" when " + monthlySalary + " <= 5000 and " + monthlySalary
 				+ " > 5000 and sealocal_epf_rates.group_name = 'GROUP C' then (" + monthlySalary + ") * 0.065 ");
 		// Group A, when monthly wage is over 20000, the employer contribution is wage * 12%
 		sb.append(" when " + monthlySalary + " > 20000 and sealocal_epf_rates.group_name = 'GROUP A' then (" + monthlySalary + ") * 0.12 ");
@@ -94,6 +83,4 @@ public class MalaysiaEPF extends AbstractPreExcutorFormulaParse {
 		return sb.toString();
 	}
 	
-
-
 }
